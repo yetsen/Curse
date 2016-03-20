b78df44dc2d56fedd811dfac50fc438f16f46db0
Yunus <yunusemretekin1@gmail.com>
2016-03-20 20:48:40 +0200

    some message

--- app/src/main/java/tr/com/orties/curse/adapters/MessageListAdapter.java
+++ app/src/main/java/tr/com/orties/curse/adapters/MessageListAdapter.java
@@ -6,6 +6,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+import tr.com.orties.curse.R;
+import tr.com.orties.curse.model.Message;
 
 /**
  * Created by Yunus on 13-Mar-16.
@@ -14,30 +20,36 @@ public class MessageListAdapter extends BaseAdapter {
 
     private Activity activity;
     private static LayoutInflater inflater = null;
+    private List<Message> messageList;
 
-    public MessageListAdapter(Activity activity) {
+    public MessageListAdapter(Activity activity, List<Message> messageList) {
         this.activity = activity;
-        inflater = (LayoutInflater) activity
+        inflater = (LayoutInflater) this.activity
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        this.messageList=messageList;
     }
 
     @Override
     public int getCount() {
-        return 0;
+        return messageList.size();
     }
 
     @Override
     public Object getItem(int i) {
-        return null;
+        return messageList.get(i);
     }
 
     @Override
     public long getItemId(int i) {
-        return 0;
+        return i;
     }
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
+        if (view == null)
+            view = inflater.inflate(R.layout.message, null);
+        TextView message = (TextView) view.findViewById(R.id.message_text);
+        message.setText(messageList.get(i).getMessage());
+        return view;
     }
 }
