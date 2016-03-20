be31f121974f5b19865df6acd2c63d08ff349cc7
Yunus <yunusemretekin1@gmail.com>
2016-03-20 20:46:42 +0200

    some message

---
+++ app/src/main/java/tr/com/orties/curse/actionbar/CustomViewPager.java
@@ -0,0 +1,43 @@
+package tr.com.orties.curse.actionbar;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * Created by Yunus on 20-Mar-16.
+ */
+public class CustomViewPager extends ViewPager {
+
+    private boolean enabled;
+
+    public CustomViewPager(Context context) {
+        super(context);
+    }
+
+    public CustomViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (this.enabled) {
+            return super.onTouchEvent(event);
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent event) {
+        if (this.enabled) {
+            return super.onInterceptTouchEvent(event);
+        }
+        return false;
+    }
+
+    public void setPagingEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+}
